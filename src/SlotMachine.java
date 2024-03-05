77a278780032537bd33c28c09c77e5b9c646a222
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-03-05 13:40:01 -0500

    prep for winnings

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -23,4 +23,15 @@ public class SlotMachine {
     public int[] pullLever() {
         return new int[] {wheels[0].spin(), wheels[1].spin(), wheels[2].spin()};
     }
+
+    /**
+     * Checks for congruent Slot Values, and sees if the User has one something.
+     * 
+     * 2 congruent slots = Prize B
+     * 3 congruent slots = Prize A
+     * @return Displayable Text to tell the user what they won.
+     */
+    public String checkWinnings() {
+        
+    }
 }
